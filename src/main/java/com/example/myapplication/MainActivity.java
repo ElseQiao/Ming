ee0f00e20adf14f13e5e134bd300c36e33e03bd4
qyl <1971658060@qq.com>
2017-04-26 17:33:07 +0800

    nothing

--- src/main/java/com/example/myapplication/MainActivity.java
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
+
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -13,31 +17,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.example.myapplication.activity.TextActivity;
+import com.example.myapplication.activity.NotesListActivity;
 import com.example.myapplication.adapter.DirsAdapter;
+import com.example.myapplication.filepicker.FilePickerActivity;
+import com.example.myapplication.filepicker.Utils;
 import com.example.myapplication.fragment.DirsDialog;
 import com.example.myapplication.model.DirsModle;
 import com.example.myapplication.model.RecyclerItemClickListener;
 import com.example.myapplication.utils.ExportUtil;
+import com.example.myapplication.utils.ImportUtil;
 
 import org.litepal.crud.DataSupport;
 
 import java.util.List;
 import java.util.Random;
+
+
 /**
  * @author qyl
  * @deprecated xi
  * */
 
 public class MainActivity extends AppCompatActivity {
+    private static final int FILE_CODE =1000;
+    private static final String TAG = "MainActivity";
     private Toolbar toolbar;
     private FloatingActionButton fb;
     private RecyclerView recyclerView;
     private SwipeRefreshLayout refresh;
     private List<DirsModle> dirs;
     private DirsAdapter adapter;
-    private int pageCount = 8;
-    private int fromId = 1;
+    private int limit=8;
+    private int offset=0;
+   // private ExecutorService singleExecutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
         StaggeredGridLayoutManager g = new StaggeredGridLayoutManager(2, OrientationHelper.VERTICAL);
 
         recyclerView.setLayoutManager(g);
-        // new LinearLayoutManager(this)
         recyclerView.setLayoutManager(g);
         recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
             @Override
@@ -63,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 if(position==dirs.size()){
                     return;
                 }
-                Intent i=new Intent(MainActivity.this, TextActivity.class);
+                Intent i=new Intent(MainActivity.this, NotesListActivity.class);
                 i.putExtra("type",dirs.get(position).getTitle());
                 i.putExtra("parent_id",dirs.get(position).getId());
                 startActivity(i);
@@ -74,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }));
 
-        footViewUpdata();
+       // footViewUpdata();
 
         refresh = (SwipeRefreshLayout) findViewById(R.id.refreshLayout);
         //设置下拉刷新的按钮的颜色
@@ -107,30 +118,31 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.action_export:
                         new ExportUtil().oneKeyExport();
                         break;
+                    case R.id.action_import:
+                        onDirPicker();
+                        break;
                 }
                 return false;
             }
         });
     }
 
-
-
     private void initData() {
+       // singleExecutor= Executors.newSingleThreadExecutor();
         refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 refresh.setRefreshing(false);
             }
         });
-        dirs = DataSupport.where("id between ? and ?", "" + fromId, "" + (pageCount + fromId)).find(DirsModle.class);
+        dirs = DataSupport.order("id desc").find(DirsModle.class);
         if (dirs == null || dirs.size() == 0) {
-            createDirs("武侠", "武侠内容");
+              createDirs("new", "默认文件");
         }
-        adapter = new DirsAdapter(this, dirs);
+        adapter = new DirsAdapter(MainActivity.this, dirs);
         recyclerView.setAdapter(adapter);
+
     }
-
-
 
     private void createDirs(String title, String introduce) {
         DirsModle m = new DirsModle();
@@ -138,8 +150,10 @@ public class MainActivity extends AppCompatActivity {
         m.setIntroduce(introduce);
         m.setColor(new Random().nextInt(10));
         if (m.save()) {
-            dirs.add(m);
-            adapter.notifyDataSetChanged();
+            dirs.add(0,m);
+            if(adapter!=null){
+                adapter.notifyDataSetChanged();
+            }
         } else {
             TipShow("创建文件失败！！！");
         }
@@ -147,18 +161,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    private void loadMore() {
-        fromId = pageCount + fromId;
-        List<DirsModle> dirsNew = DataSupport.where("id between ? and ?", "" + fromId, "" + (pageCount + fromId)).find(DirsModle.class);
 
-        if (dirsNew==null||dirsNew.size() == 0) {
-            fromId = fromId - pageCount;
-            TipShow("没有更多数据了");
-            return;
-        }
-        dirs.addAll(dirsNew);
-        adapter.notifyDataSetChanged();
-    }
 
     private void TipShow(String tip) {
         Snackbar.make(fb, tip, Snackbar.LENGTH_SHORT).show();
@@ -229,5 +232,57 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main,menu);
         return true;
+    }
+
+    private void onDirPicker() {
+        // This always works
+        Intent i = new Intent(this, FilePickerActivity.class);
+
+        // Set these depending on your use case. These are the defaults.
+        i.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, true);
+        i.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
+        i.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
+        i.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
+        startActivityForResult(i, FILE_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_CODE && resultCode == Activity.RESULT_OK) {
+            // Use the provided utility method to parse the result
+            List<Uri> uris = Utils.getSelectedFilesFromResult(data);
+            // Do something with the result...
+            ImportUtil i=new ImportUtil();
+            i.saveDirs(uris, new ImportUtil.ImportResultListener() {
+                @Override
+                public void success() {
+                    importMore();
+                }
+            });
+        }
+    }
+
+    private void importMore() {
+        refresh.setRefreshing(true);
+        List<DirsModle> newDirs=DataSupport.order("id desc").find(DirsModle.class);
+        if (newDirs == null || newDirs.size() == 0) {
+            return;
+        }
+        dirs.clear();
+        dirs.addAll(newDirs);
+        adapter.notifyDataSetChanged();
+        refresh.setRefreshing(false);
+    }
+
+    private void loadMore() {
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+      //  recyclerView.removeOnItemTouchListener();
     }
 }
